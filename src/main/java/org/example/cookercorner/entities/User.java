ad70ad5923c1e9217dde6896316ec93f8edeb4ec
akzholbek <akzholbek0096@gmail.com>
2024-03-14 08:03:29 +0000

    added user and role entities

---
+++ src/main/java/org/example/cookercorner/entities/User.java
@@ -0,0 +1,92 @@
+package org.example.cookercorner.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Email field should not be blank")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Username field should not be blank")
+    @Column(nullable = false, unique = true)
+    private String username;
+
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean isEnabled;
+
+    @CreationTimestamp
+    private Date createdDate;
+
+    @UpdateTimestamp
+    private Date updatedDate;
+
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user's_roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
+    )
+    private List<Role> roles = new ArrayList<>();
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnabled;
+    }
+
+}
